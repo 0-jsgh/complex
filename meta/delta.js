62a6b14828a9ff4edf9acd217235e9a0ad833744
Johan Gomez <142572116+0-jsgh@users.noreply.github.com>
2024-07-17 14:19:35 -0500

    Update delta.js

--- meta/delta.js
+++ meta/delta.js
@@ -51,8 +51,8 @@ function convert() {
         resultContainer.innerHTML += `<p>Zb: ${result.zb}</p>`;
         resultContainer.innerHTML += `<p>Zc: ${result.zc}</p>`;
     } else {
-        resultContainer.innerHTML += `<p>Z1: ${result.z1}</p>`;
-        resultContainer.innerHTML += `<p>Z2: ${result.z2}</p>`;
-        resultContainer.innerHTML += `<p>Z3: ${result.z3}</p>`;
+        resultContainer.innerHTML += `<p>Za: ${result.z1}</p>`;
+        resultContainer.innerHTML += `<p>Zb: ${result.z2}</p>`;
+        resultContainer.innerHTML += `<p>Zc: ${result.z3}</p>`;
     }
 }
